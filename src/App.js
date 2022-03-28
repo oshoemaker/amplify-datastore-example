2233235bfc4bbf093980dc6c5fddb02aa3b614b4
Owen Shoemaker <owen.shoemaker@prometheanworld.com>
2022-03-28 09:52:15 -0700

    Fix the project with better functionality and updates. Packages out of date.

--- src/App.js
+++ src/App.js
@@ -2,53 +2,89 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { DataStore, Predicates } from "@aws-amplify/datastore";
+import { Amplify, DataStore, Predicates } from "aws-amplify";
 import { Post, PostStatus } from "./models";
 
-function App() {
-  const [form, updateForm] = useState({ title: '', rating: '' })
-  async function query() {
-    const posts = await DataStore.query(Post);
-    console.log('posts: ', posts)
-    const original = await DataStore.query(Post, "4d5a08f3-d0ac-42bd-a19e-170991a4d79b");
+//Use next two lines only if syncing with the cloud
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
-    // await DataStore.save(
-    //   Post.copyOf(original, updated => {
-    //     updated.title = `title ${Date.now()}`;
-    //     updated.status = PostStatus.ACTIVE
-    //   })
-    // );
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const onQuery = async () => {
+    //const posts = await DataStore.query(Post, (c) => c.rating("gt", 4));
+    const posts = await DataStore.query(Post);
+    setItems(posts);
+    console.log(posts);
   }
-  async function create() {
-    const postData = {...form, status: PostStatus.INACTIVE}
+
+  const onCreate = async () => {
     await DataStore.save(
-      new Post(postData)
+      new Post({
+        title: `New title ${Date.now()}`,
+        rating: (function getRandomInt(min, max) {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min)) + min;
+        })(1, 7),
+        status: PostStatus.ACTIVE,
+      })
     );
-    console.log('successfully created new post')
-    updateForm({ title: '', rating: '' })
+
+    await onQuery();
   }
+
+  const onDeleteAll = async () => {
+    await DataStore.delete(Post, Predicates.ALL);
+    await onQuery();
+  }
+
+  const onDeleteOne = async () => {
+    await DataStore.delete(items[0]);
+    await onQuery();
+  }
+
   useEffect(() => {
-    query()
+    const init = async () => {
+      try {
+        setLoading(true);
+        await onQuery();
+        const subscription = DataStore.observe(Post).subscribe((msg) => {
+          console.log('Model: ',msg.model);
+          console.log('opType: ', msg.opType);
+          console.log('element: ', msg.element);
+          onQuery();
+        });
+    
+        return () => subscription.unsubscribe();
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    init();
   }, []);
+
+  const count = loading ? 'loading' : items.length;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <input type="button" value="QUERY" onClick={query} />
+          <input type="button" value="NEW" onClick={onCreate} />
+          <input type="button" value="DELETE ONE" onClick={onDeleteOne} />
+          <input type="button" value="DELETE ALL" onClick={onDeleteAll} />
         </div>
-        <input
-          value={form.title}
-          placeholder="title"
-          onChange={e => updateForm({ ...form, 'title': e.target.value })}
-        />
-        <input
-          value={form.rating}
-          placeholder="rating"
-          onChange={e => updateForm({ ...form, 'rating': parseInt(e.target.value) })}
-        />
-        <button onClick={create}>Create Post</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          Number of items: { count }
+        </div>
+
       </header>
     </div>
   );
